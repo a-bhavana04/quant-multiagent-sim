362345482ba6d54ff69efb3b2bd9b6206edce36d
Bhavana Anand <91405053+a-bhavana04@users.noreply.github.com>
2025-07-17 14:42:12 +0530

    Update training_agent.py

--- app/training_agent.py
+++ app/training_agent.py
@@ -3,5 +3,5 @@ from stable_baselines3 import PPO
 
 env = MarketEnv()
 model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=100_000)  # adjust timesteps if needed
+model.learn(total_timesteps=100_000) 
 model.save("trained_quant_agent")
