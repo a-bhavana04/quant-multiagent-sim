dff56ebbf1ed391e5d4a72bdf0129ad9729830bd
Bhavana Anand <91405053+a-bhavana04@users.noreply.github.com>
2025-07-17 14:45:49 +0530

    Update market_env.py

--- app/rl/market_env.py
+++ app/rl/market_env.py
@@ -7,7 +7,7 @@ class MarketEnv(gym.Env):
     def __init__(self):
         super(MarketEnv, self).__init__()
         self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=(5,), dtype=np.float32)
-        self.action_space = gym.spaces.Discrete(3)  # 0=Buy, 1=Hold, 2=Sell
+        self.action_space = gym.spaces.Discrete(3)  
         self.reset()
 
     def reset(self):
@@ -19,26 +19,26 @@ class MarketEnv(gym.Env):
     def _get_obs(self):
         recent = self.prices[-5:]
         while len(recent) < 5:
-            recent.insert(0, 100.0)  # pad to ensure 5 prices
+            recent.insert(0, 100.0) 
         returns = np.diff(recent) / np.array(recent[:-1])
         obs = np.concatenate([[recent[-1]], returns])
         return obs.astype(np.float32)
 
     def step(self, action):
         reward = 0
-        next_price = self.prices[-1] + np.random.normal(0, 1)  # simulate price
+        next_price = self.prices[-1] + np.random.normal(0, 1)  
         self.prices.append(next_price)
 
-        if action == 0:  # Buy
+        if action == 0:  
             self.position += 1
             self.cash -= next_price
-        elif action == 2:  # Sell
+        elif action == 2:  
             self.position -= 1
             self.cash += next_price
 
         unrealized_pnl = self.position * next_price
         total_value = self.cash + unrealized_pnl
-        reward = total_value - 10000  # reward = net profit
+        reward = total_value - 10000 
 
         done = False
         return self._get_obs(), reward, done, {}
